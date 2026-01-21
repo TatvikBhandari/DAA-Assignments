18baa24d21924f058892da1a2921f1b0763a08bc
Tatvik Bhandari <tatvikbhandari@gmail.com>
2026-01-22 05:23:10 +0530

    Update 1.cpp

--- Assignment 1/Additional/1.cpp
+++ Assignment 1/Additional/1.cpp
@@ -1,9 +1,31 @@
-//text wrap
+//Tutorial question
 #include <iostream>
+#include <algorithm>
+#include <unordered_map>
 using namespace std;
 
-int 
 int main() {
-    
+    int n;
+    cin >> n;
+
+    int arr[n];
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    sort(arr, arr + n);
+
+    unordered_map<int, int> rank;
+    for(int i = 0; i < n; i++) {
+        rank[arr[i]] = i + 1;  
+    }
+
+    int q;
+    cin >> q;
+    while(q--) {
+        int x;
+        cin >> x;
+        cout << rank[x] << endl;
+    }
+
     return 0;
 }
